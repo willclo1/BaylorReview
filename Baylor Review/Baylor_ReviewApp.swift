a5ff3040379fbd65fdce6d687f57c7483e7725f6
willclo1 <115596953+willclo1@users.noreply.github.com>
2025-08-06 22:54:28 -0500

    Initial Commit

---
+++ Baylor Review/Baylor_ReviewApp.swift
@@ -0,0 +1,32 @@
+//
+//  Baylor_ReviewApp.swift
+//  Baylor Review
+//
+//  Created by Will Clore on 8/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Baylor_ReviewApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
